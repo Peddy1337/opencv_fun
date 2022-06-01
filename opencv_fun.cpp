ec69ef6274571339c4a3f781443914967747e297
Peddy1337 <64089550+Peddy1337@users.noreply.github.com>
2022-06-01 20:59:25 +0200

    [impl] capture webcam feed and enable filtering and color conversion

--- opencv_fun.cpp
+++ opencv_fun.cpp
@@ -1,20 +1,61 @@
-// opencv_fun.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+
+using namespace cv;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    VideoCapture capture;
+    bool run = true, hsv = false, blurring = false;
+    Size filterSize = Size(10, 10);
+    capture.open(0);
+    while (run)
+    {
+        Mat frame;
+        capture >> frame;
+
+        if (frame.empty())
+        {
+            break;
+        }
+
+        if (hsv)
+        {
+            // converts the image of the frame to hsv
+            cvtColor(frame, frame, COLOR_BGR2HSV);
+        }
+
+        if (blurring)
+        {
+            blur(frame, frame, filterSize);
+        }
+
+        imshow("Testfeed", frame);
+
+        switch (waitKey(5))
+        {
+        case 'h':
+            hsv = !hsv;
+            break;
+        case 'b':
+            blurring = !blurring;
+            break;
+        case '+':
+            filterSize = Size(filterSize.width + 1, filterSize.height + 1);
+            break;
+        case '-':
+            if (filterSize.height > 1)
+            {
+                filterSize = Size(filterSize.width - 1, filterSize.height - 1);
+            }
+            break;
+        case 27:
+            run = false;
+            break;
+        }
+    }
+    capture.release();
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
